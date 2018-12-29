ebf614c7118311291d84fc63d9c1339b127942be
Satoshi Numata <numata@oecu.jp>
2018-12-30 02:16:40 +0900

    Houstonの迷路生成アルゴリズムを実装しました。

--- src/Maze/Maze_AldousBroder.hpp
+++ src/Maze/Maze_AldousBroder.hpp
@@ -17,6 +17,14 @@
  */
 Maze    *CreateMaze_AldousBroder(int xSize, int ySize);
 
+/**
+    Aldous-Broderのアルゴリズムによる迷路生成の実装です。
+    Houstonのアルゴリズムのために実装部分を分けています。
+    @param  maze    迷路
+    @param  cellCountThreshold  このアルゴリズムで処理するセルの個数
+ */
+void AldousBroder_Impl(Maze *maze, int cellCountThreshold);
+
 
 #endif /* Maze_AldousBroder_hpp */
 
