1e2d93c90fbfc458f6f59c3bd29c4e688f61ea40
Satoshi Numata <numata@oecu.jp>
2019-01-08 01:57:49 +0900

    Recursive Backtrackingの迷路生成アルゴリズムを、100x100マス程度位上のサイズの迷路でも動作できるように、ヒープ上に確保したスタックを使って再帰処理を行うように修正しました。

--- src/Maze/Maze_RecursiveBacktracking.cpp
+++ src/Maze/Maze_RecursiveBacktracking.cpp
@@ -13,24 +13,40 @@ using namespace std;
 
 
 /**
+    ヒープ上での再帰処理のためのスタックフレーム
+ */
+static vector<CellPoint> stack;
+
+
+/**
     Recurvsive Backtrackingの再帰関数
     @param  maze    処理対象の迷路
     @param  pos     処理を開始するセル座標
  */
-static void CarvePassage(Maze *maze, const CellPoint& pos)
+static void CarvePassage(Maze *maze)
 {
+    CellPoint pos = *(stack.end() - 1);
+
     // 方向をランダムに格納した配列を用意する
     vector<Direction> dirList = MakeAllDirectionsList_shuffled();
 
     // それぞれの方向に掘り進める
+    bool isFinished = true;
     for (auto dir : dirList) {
         CellPoint nextPos = pos.Move(dir);
         if (maze->IsValidCell(nextPos) && maze->GetCellTag(nextPos) == 1) {
             maze->RemoveWall(pos, dir);
             maze->SetCellTag(nextPos, 0);
             maze->Draw();
-            CarvePassage(maze, nextPos);
+
+            stack.push_back(nextPos);
+            isFinished = false;
+            break;
         }
+    }
+
+    if (isFinished) {
+        stack.erase(stack.end() - 1);
     }
 }
 
@@ -50,7 +66,10 @@ Maze *CreateMaze_RecurvsiveBacktracking(int xSize, int ySize)
     CellPoint p;
     p.x = random() % maze->GetXSize();
     p.y = random() % maze->GetYSize();
-    CarvePassage(maze, p);
+    stack.push_back(p);
+    while (stack.size() > 0) {
+        CarvePassage(maze);
+    }
 
     return maze;
 }
